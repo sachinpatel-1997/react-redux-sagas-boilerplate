886e0ee8946e882dd59bdde0df4bd8f21c3b30ee
Noopur Chhajed <nchhajed@isystango.com>
2022-04-12 17:04:38 +0530

    React ace editor

---
+++ src/components/Sqllab/AsyncAceEditor.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AceEditor from "react-ace";
+
+const AsyncAceEditor = () => {
+
+   return(
+   <div>
+      Sqllab Editor
+      
+   </div>
+   )
+}
+
+export default AsyncAceEditor
--- src/components/Sqllab/index.js
+++ src/components/Sqllab/index.js
@@ -1,6 +1,36 @@
-import React from 'react'
+import React from 'react';
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/mode-mysql";
+import "ace-builds/src-noconflict/theme-tomorrow";
+import "ace-builds/src-noconflict/ext-language_tools"
+import { Button } from 'react-bootstrap';
+
 const SqllabComponent = () => {
-   return(<div>sqllab</div>)
+   const onChange = (newValue) => {
+      console.log("change", newValue);
+   }
+
+   return(
+   <div>
+      Sqllab Editor
+      <AceEditor
+         mode="mysql"
+         theme="tomorrow"
+         fontSize={16}
+         onChange={onChange}
+         name="UNIQUE_ID_OF_DIV"
+         editorProps={{ $blockScrolling: true }}
+         setOptions={{
+         enableBasicAutocompletion: true,
+         enableLiveAutocompletion: true,
+         enableSnippets: true
+         }}
+      />
+      <Button>Run</Button>
+      <Button variant="outline-primary">Save Query</Button>
+   </div>
+   )
 }
 
 export default SqllabComponent
