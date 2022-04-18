6b2f27996823358b5a0eb9ac863cbceaf6a1cc70
Mahip <mmodh@isystango.com>
2022-04-18 12:26:43 +0530

    Work on the overview component position, code cleanup

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,11 +2,6 @@ import React, { Fragment } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { interceptor } from 'utils/interceptor'
 import SqllabContainer from 'containers/Sqllab'
-import EdgesFlow from 'containers/EdgesFlow'
-import ReactDBDgm from 'containers/ReactDBDgm'
-import DbDiagram from 'containers/DbDiagram'
-// import ReactDiagrams from 'containers/ReactDiagrams'
-import GoJS from 'containers/Gojs'
 import GoJsReact from 'containers/GoJsReact'
 
 
@@ -16,13 +11,7 @@ export default function App() {
     <Fragment>
       <Switch>
         <Route exact path="/sqllab" component={SqllabContainer} />
-        <Route exact path="/edge" component={EdgesFlow} />
-        <Route exact path="/react-database-daigram" component={ReactDBDgm} />
-        <Route exact path="/dbdaigram" component={DbDiagram} />
-        <Route exact path="/gojs" component={GoJS} />
-        <Route exact path="/goJsReact" component={GoJsReact} />
-        {/* <Route exact path="/reactDiagrams" component={ReactDiagrams} /> */}
-        
+        <Route exact path="/erd" component={GoJsReact} />        
       </Switch>
     </Fragment>
   )
